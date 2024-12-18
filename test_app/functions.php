44d6374117aeaab989d7dadb8e9a31ad52b16114
Your Name <you@example.com>
2024-12-18 13:19:33 +0900

    口頭レニュー2

--- test_app/functions.php
+++ test_app/functions.php
@@ -1,12 +1,36 @@
 <?php
 require_once('connection.php');
 
-function createData($post)
-{
-    createTodoData($post['content']);
-}
-
 function getTodoList()
 {
     return getAllRecords();
+}
+
+function getSelectedTodo($id)
+{
+    return getTodoTextById($id); 
+}
+
+function savePostedData($post)
+{
+    $path = getRefererPath();
+    switch ($path) {
+        case '/new.php':
+            createTodoData($post['content']);
+            break;
+        case '/edit.php':
+            updateTodoData($post);
+            break;
+        case '/index.php': // 追記
+            deleteTodoData($post['id']); // 追記
+            break; // 追記
+        default:
+            break;
+    }
+}
+
+function getRefererPath()
+{
+    $urlArray = parse_url($_SERVER['HTTP_REFERER']);
+    return $urlArray['path'];
 }
